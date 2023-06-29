f0b985c828f109b57a283e19d8fd5322a30d0212
Leandro Martinez <lmartine@unicamp.br>
2023-06-29 09:36:33 -0300

    remove unnecessary imports

--- test/Testing.jl
+++ test/Testing.jl
@@ -1,13 +1,4 @@
 module Testing
-    using ..Stride
-    using Chemfiles
-    using PDBTools
-
     export data_dir
-
     data_dir = joinpath(@__DIR__, "data")
-
-    #pdbfile = joinpath(data_dir, "Gromacs/system.pdb")
-    #trajectory = Trajectory(joinpath(data_dir, "Gromacs/trajectory.xtc"))
-
 end
