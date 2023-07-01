d0dcc0104cfaed33f5c478905cfcd3286bc94e63
Leandro Martinez <lmartine@unicamp.br>
2023-06-30 23:15:41 -0300

    fix pdb header and tests for dssp, by default

--- src/dssp.jl
+++ src/dssp.jl
@@ -18,12 +18,33 @@ variable can be set to the full path of the executable.
 """
 function dssp_run end
 
-const dssp_init_line =
-    "  #  RESIDUE AA STRUCTURE BP1 BP2  ACC     N-H-->O    O-->H-N    N-H-->O    O-->H-N    TCO  KAPPA ALPHA  PHI   PSI    X-CA   Y-CA   Z-CA"
+function dssp_pdb_header() 
+    strip("""
+          HEADER
+          TITLE     TITLE
+          COMPND    MOL_ID: 1;
+          SOURCE    MOL_ID: 1;
+          KEYWDS    KEYWORDS
+          EXPDTA    X-RAY DIFFRACTION
+          AUTHOR    ABC
+          CRYST1
+          """)
+end
 
-function dssp_run(pdb_file::String)
+
+function dssp_run(pdb_file::String; fix_header=true)
+    # if the header is not in the correct format, dssp will fail
+    if fix_header
+        atoms = PDBTools.readPDB(pdb_file, "protein")
+        tmp_file = tempname()*".pdb"
+        PDBTools.writePDB(atoms, tmp_file; header=dssp_pdb_header(), footer=nothing)
+    else
+        tmp_file = pdb_file
+    end
     # Run dssp on the pdb file
-    dssp_raw_data = readchomp(pipeline(`$dssp_executable --output-format dssp $pdb_file`))
+    dssp_init_line =
+    "  #  RESIDUE AA STRUCTURE BP1 BP2  ACC     N-H-->O    O-->H-N    N-H-->O    O-->H-N    TCO  KAPPA ALPHA  PHI   PSI    X-CA   Y-CA   Z-CA"
+    dssp_raw_data = readchomp(pipeline(`$dssp_executable --output-format dssp $tmp_file`))
     ssvector = SSData[]
     data_begin = false
     residue = 0
@@ -38,10 +59,10 @@ function dssp_run(pdb_file::String)
         push!(ssvector,
             SSData(
                 resname = PDBTools.threeletter(line[14]),
-                chain = "$line[12]",
+                chain = "$(line[12])",
                 residue = residue,
                 resnum = parse(Int, line[6:10]),
-                sscode = "$line[17]",
+                sscode = "$(line[17])",
                 phi = parse(Float64, line[104:109]),
                 psi = parse(Float64, line[110:115]),
                 kappa = parse(Float64, line[92:97]),
@@ -58,25 +79,25 @@ end
     ss = dssp_run(pdbfile)
     @test length(ss) == 510
     @test ss_composition(ss) == Dict{String, Int}(
-        "310 helix" => 21, 
-        "bend" => 0, 
-        "turn" => 70, 
-        "helix" => 263, 
-        "beta strand" => 77, 
-        "alpha helix" => 242, 
-        "pi helix" => 0, 
+        "310 helix" => 20, 
+        "bend" => 26, 
+        "turn" => 58, 
+        "helix" => 250, 
+        "beta strand" => 74, 
+        "alpha helix" => 220, 
+        "pi helix" => 10, 
         "beta bridge" => 4, 
-        "strand" => 81, 
-        "coil" => 96,
-        "loop" => 96,
+        "strand" => 78, 
+        "coil" => 0,
+        "loop" => 98,
     )
 end
 
 function dssp_run(atoms::AbstractVector{<:PDBTools.Atom})
     tmp_file = tempname()*".pdb"
     @show tmp_file
-    PDBTools.writePDB(atoms, tmp_file)
-    ss = dssp_run(tmp_file)
+    PDBTools.writePDB(atoms, tmp_file; header=dssp_pdb_header())
+    ss = dssp_run(tmp_file; fix_header=false)
     rm(tmp_file)
     return ss
 end
@@ -89,16 +110,16 @@ end
     ss = dssp_run(atoms)
     @test length(ss) == 255 
     @test ss_composition(ss) == Dict{String, Int}(
-        "310 helix"   => 11,
-        "bend"        => 0,
-        "turn"        => 34,
-        "helix"       => 132,
-        "beta strand" => 39,
-        "alpha helix" => 121,
-        "pi helix"    => 0,
+        "310 helix"   => 10,
+        "bend"        => 13,
+        "turn"        => 29,
+        "helix"       => 125,
+        "beta strand" => 37,
+        "alpha helix" => 110,
+        "pi helix"    => 5,
         "beta bridge" => 2,
-        "strand"      => 41,
-        "coil"        => 48,
-        "loop"        => 48,
+        "strand"      => 39,
+        "coil"        => 0,
+        "loop"        => 49,
     )
 end
