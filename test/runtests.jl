aad66a26ee219b931b20473174e45b5bc5ce238a
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-07-13 09:26:46 +0200

    Run Aqua.test_all during tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using TestItemRunner
 @run_package_tests
 
+@testitem "Aqua.test_all" begin
+    import Aqua
+    Aqua.test_all(ProteinSecondaryStructures)
+end
+
 @testitem "from pdb file" begin
     using ProteinSecondaryStructures.Testing
     for i in eachindex(examples)
